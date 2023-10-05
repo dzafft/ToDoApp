bf157c180c2f521250a5d227a8dd016cc41db974
Dietrich <111803883+dzafft@users.noreply.github.com>
2023-10-05 03:36:43 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Note from './components/Note.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Notes Website</h1>
+      <p>Create and delete notes</p>
+    </header>
+    <main>
+      <Note />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+/* Styles scoped to this component */
+
+/* Style the header element */
+header {
+  background-color: #007acc;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+
+/* Style the h1 element inside the header */
+header h1 {
+  font-size: 36px;
+}
+
+/* Style the p element inside the header */
+header p {
+  font-size: 18px;
+}
+
+/* Style the main content area */
+main {
+  padding: 20px;
+}
+
+
+</style>
